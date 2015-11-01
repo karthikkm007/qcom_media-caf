85660b7997add8c2a8df316376ccbcad1cb74e5b
Steve Kondik <steve@cyngn.com>
2015-11-01 02:50:41 -0800

    media: Fix issues when building with Clang
    Change-Id: If1a690ac7a2c254afe444b617439ba9e893c2502

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -11801,8 +11801,8 @@ OMX_ERRORTYPE omx_vdec::describeColorFormat(OMX_PTR pParam) {
     DEBUG_PRINT_LOW("  FrameWidth x FrameHeight : %d x %d", params->nFrameWidth, params->nFrameHeight);
     DEBUG_PRINT_LOW("  YWidth x YHeight : %d x %d", img->mWidth, img->mHeight);
     for (size_t i = 0; i < img->mNumPlanes; ++i) {
-        DEBUG_PRINT_LOW("    Plane[%d] : offset=%d / xStep=%d / yStep = %d",
-                i, img->mPlane[i].mOffset, img->mPlane[i].mColInc, img->mPlane[i].mRowInc);
+        DEBUG_PRINT_LOW("    Plane[%u] : offset=%u / xStep=%u / yStep = %u",
+                (int)i, img->mPlane[i].mOffset, img->mPlane[i].mColInc, img->mPlane[i].mRowInc);
     }
     return OMX_ErrorNone;
 #endif //FLEXYUV_SUPPORTED
